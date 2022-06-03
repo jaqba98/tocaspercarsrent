0df5da539ccb06a0369419fee17d3ef4416e85b0
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2022-06-03 12:39:05 +0200

    create footer

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/pages/index.html'));
+    res.sendFile(path.join(__dirname, '/src/index.html'));
 });
 
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
