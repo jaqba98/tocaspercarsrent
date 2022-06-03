de574cf7833d6c96a8b34b09a9a5df91fed6c212
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2022-06-03 11:37:10 +0200

    create first html file

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/pages/index.html'));
+});
 
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
