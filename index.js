1366d6cf1e7b0e31281c6d65f3eddd214f4199e4
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2022-06-03 14:41:42 +0200

    add video

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/src/index.html'));
