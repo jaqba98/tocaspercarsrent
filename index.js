39587c844c2097981c06f42decd28d515c01dcf7
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2022-06-03 22:02:08 +0200

    refactor the configuration files

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8080;
+const src = path.join(__dirname, '/src');
+const indexHTMLFile = path.join(__dirname, '/src/index.html');
 
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(src));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/index.html'));
-});
+app.get('/', (req, res) => res.sendFile(indexHTMLFile));
+app.get('/strona-glowna', (req, res) => res.sendFile(indexHTMLFile));
 
-app.listen(port, () => console.log(`Server listening on port: ${port}`));
+app.listen(port, () => console.log(`Server listening on: http://localhost:${port}`));
