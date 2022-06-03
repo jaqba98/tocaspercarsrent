b9ff9876860ecf45ca40ab0dc3b2c46638890225
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2022-06-03 11:24:40 +0200

    change server run message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(process.env.PORT || port, () => console.log('Server running'));
+app.listen(port, () => console.log(`Server listening on port: ${port}`));
