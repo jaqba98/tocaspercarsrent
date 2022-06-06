376845ce4036d17a4a8279ca0f28037858f90b5d
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2022-06-06 13:00:06 +0200

    add our-cars page

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const app = express();
 const port = process.env.PORT || 8080;
 const src = path.join(__dirname, '/src');
 const indexHTMLFile = path.join(__dirname, '/src/index.html');
+const ourCarsHTMLFile = path.join(__dirname, '/src/our-cars.html');
 
 app.use(express.static(src));
 
 app.get('/', (req, res) => res.sendFile(indexHTMLFile));
 app.get('/strona-glowna', (req, res) => res.sendFile(indexHTMLFile));
+app.get('/nasze-samochody', (req, res) => res.sendFile(ourCarsHTMLFile));
 
 app.listen(port, () => console.log(`Server listening on: http://localhost:${port}`));
