cf428f6b2c820b39811717329f92322b94c78d6f
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2022-06-06 13:25:46 +0200

    ass gallery page

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const port = process.env.PORT || 8080;
 const src = path.join(__dirname, '/src');
 const indexHTMLFile = path.join(__dirname, '/src/index.html');
 const ourCarsHTMLFile = path.join(__dirname, '/src/our-cars.html');
+const galleryHTMLFile = path.join(__dirname, '/src/gallery.html');
 
 app.use(express.static(src));
 
 app.get('/', (req, res) => res.sendFile(indexHTMLFile));
 app.get('/strona-glowna', (req, res) => res.sendFile(indexHTMLFile));
 app.get('/nasze-samochody', (req, res) => res.sendFile(ourCarsHTMLFile));
+app.get('/galeria', (req, res) => res.sendFile(galleryHTMLFile));
 
 app.listen(port, () => console.log(`Server listening on: http://localhost:${port}`));
