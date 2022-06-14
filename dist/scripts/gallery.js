57d2934935f804ea87c57427b600f85f660f3d05
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2022-06-14 13:44:30 +0200

    optimalize the dist

---
+++ dist/scripts/gallery.js
@@ -0,0 +1,1 @@
+const galleryElements=document.getElementById("galleryElements"),result=cars.map(car=>({id:car.id,name:car.name,photos:car.photos})).map(car=>car.photos.map(photo=>({id:car.id,name:car.name,photo:photo}))).flat().forEach(car=>galleryElements.innerHTML+=`<img src='assets/our-cars/${car.id}/${car.photo}' alt='${car.name}'>`);
